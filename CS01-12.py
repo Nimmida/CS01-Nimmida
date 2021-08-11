63bd18d7932c48029c92bacfdd62ef9127139863
Nimmida <87053880+Nimmida@users.noreply.github.com>
2021-08-11 12:10:43 +0700

    for in range

---
+++ CS01-12.py
@@ -0,0 +1,9 @@
+Num = int(input("Enter your loop:"))
+total=[]
+for i in range(Num) :
+    data=int(input("Enter your number:"))
+    total+=[data]
+x-(min(total))
+y-(max(total))
+print(x)
+print(y)
